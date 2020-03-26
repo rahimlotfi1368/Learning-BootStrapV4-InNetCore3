2b0d8cf4b735ac24484003d005aed54b250b456e
rahim lotfi <sinuhe_1368@yahoo.com>
2020-03-27 01:46:39 +0430

    Add project files.

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,14 @@
+﻿namespace LearningBootstrapV4_NetCore3._1.Controllers
+{
+    public class HomeController : Microsoft.AspNetCore.Mvc.Controller
+    {
+        public HomeController():base()
+        {
+
+        }
+        public Microsoft.AspNetCore.Mvc.IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
